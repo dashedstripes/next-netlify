a95e91d51a92ac53442f5b97d3a7f286291f96c2
Adam Gray <adamgray@Adams-MacBook-Pro.local>
2023-05-03 16:36:01 -0500

    use Link instead of a tag to appease next

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -1,8 +1,10 @@
+import Link from 'next/link';
+
 const Header = () => {
   return (
     <header className="bg-slate-200">
       <div className="container mx-auto px-4 py-8 flex justify-between items-center">
-        <a href="/">ausnext</a>
+        <Link href="/">ausnext</Link>
         <p className='p-2 text-sm bg-slate-500 text-slate-100 rounded'>{`{{USER_LOCATION}}`}</p>
       </div>
     </header>
