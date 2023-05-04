4ed0e5a66642ba99f6dbeba677b4fc7a4336e36e
Adam Gray <adamgray@Adams-MacBook-Pro.local>
2023-05-04 10:16:50 -0500

    minor styling

--- pages/components/PostList.tsx
+++ pages/components/PostList.tsx
@@ -21,12 +21,12 @@ const PostsList: React.FC<Props> = ({ posts }) => {
   const renderedPosts = posts?.map((post: Post) => (
     <Link key={post.title} className='w-full' href={`/posts/${post.slug}`}>
       <img src={post.image.url} alt={post.title} width={1000} height={600} className="rounded-xl"/>
-      <h2 className="font-bold text-2xl">{post.title}</h2>
+      <h2 className="font-bold text-2xl py-4">{post.title}</h2>
     </Link>
   ))
 
   return (
-    <div className='container m-auto grid grid-cols-3 gap-8 py-8'>
+    <div className='container m-auto grid grid-cols-3 gap-8 p-8'>
       {renderedPosts}
     </div>
   )
