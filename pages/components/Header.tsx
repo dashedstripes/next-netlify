a5f4b31c1496848a31c76f484a9476dcbb0ab266
Adam Gray <adamgray@Adams-MacBook-Pro.local>
2023-05-09 15:45:04 -0500

    update header

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header className="bg-slate-200">
       <div className="container mx-auto px-4 py-8 flex justify-between items-center">
-        <Link href="/">ausnext</Link>
+        <Link href="/">ausnext - test branch</Link>
         <p className='p-2 text-sm bg-slate-500 text-slate-100 rounded'>{`{{USER_LOCATION}}`}</p>
       </div>
     </header>
